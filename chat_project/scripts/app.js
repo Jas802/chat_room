1ecb8634abac6d9791a4de9c0cfc2aa18161895c
Jas802 <jas.spencer414@gmail.com>
2020-11-16 12:25:08 -0500

    added new chat function

--- chat_project/scripts/app.js
+++ chat_project/scripts/app.js
@@ -1,5 +1,15 @@
 //dom queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+//add new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(error => console.log(error))
+});
 
 //class instances
 const chatUI = new ChatUI(chatList);
